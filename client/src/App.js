a4c958b688344b78aff2e136bf47430ea0678dd9
Leon <leon.karho.chung@gmail.com>
2019-11-05 12:38:37 -0500

    include cssbaseline for consistent elements - add /signup routing

--- client/src/App.js
+++ client/src/App.js
@@ -1,20 +1,21 @@
-import React from "react";
-import { MuiThemeProvider } from "@material-ui/core";
-import { BrowserRouter, Route } from "react-router-dom";
+import React from 'react'
+import { MuiThemeProvider, CssBaseline } from '@material-ui/core'
+import { BrowserRouter, Route } from 'react-router-dom'
 
-import { theme } from "./themes/theme";
-import LandingPage from "./pages/Landing";
+import { theme } from './themes/theme'
 
-import "./App.css";
+import './App.css'
+import SignUp from './pages/SignUp'
 
 function App() {
   return (
     <MuiThemeProvider theme={theme}>
       <BrowserRouter>
-        <Route path="/" component={LandingPage} />
+        <CssBaseline />
+        <Route path="/signup" component={SignUp} />
       </BrowserRouter>
     </MuiThemeProvider>
-  );
+  )
 }
 
-export default App;
+export default App
