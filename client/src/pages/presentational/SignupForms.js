bb8b1c5c6ef00d52a76f2427f05daf7cdea238f1
Leon <leon.karho.chung@gmail.com>
2019-11-05 13:23:23 -0500

    create styles obj

--- client/src/pages/presentational/SignupForms.js
+++ client/src/pages/presentational/SignupForms.js
@@ -1,55 +1,68 @@
 import React from 'react'
 import { Formik } from 'formik'
-import {
-  FormControl,
-  InputLabel,
-  Input,
-  Button,
-  TextField
-} from '@material-ui/core'
+import { FormControl, InputLabel, Input, Button } from '@material-ui/core'
+
+const styles = {
+  formContainer: { display: 'flex', flexDirection: 'column' },
+  formControl: { marginBottom: '1rem' },
+  button: {
+    borderRadius: '9999px',
+    display: 'inline-block',
+    textDecoration: 'none',
+    backgroundColor: '#111',
+    color: 'white',
+    paddingTop: '0.5rem',
+    paddingBottom: '0.5rem',
+    paddingLeft: '1rem',
+    paddingRight: '1rem',
+    marginBottom: '.5rem',
+    fontSize: '1rem'
+  }
+}
 
 const SignupForms = () => {
   return (
     <Formik
       initialValues={{ fullName: '', email: '', password: '' }}
       onSubmit={values => {
-        console.log('values', values)
+        console.log(values)
       }}
     >
       {props => {
         return (
-          <form
-            style={{ display: 'flex', flexDirection: 'column' }}
-            onSubmit={props.handleSubmit}
-          >
-            <FormControl style={{ marginBottom: '1rem' }}>
-              <InputLabel shrink={true}>Your name</InputLabel>
-              <Input
-                type="text"
-                name="fullName"
-                value={props.values.fullName}
-                onChange={props.handleChange}
-              />
-            </FormControl>
-            <FormControl style={{ marginBottom: '1rem' }}>
-              <InputLabel shrink={true}>Email Address</InputLabel>
-              <Input
-                type="email"
-                name="email"
-                value={props.values.email}
-                onChange={props.handleChange}
-              />
-            </FormControl>
-            <FormControl style={{ marginBottom: '1rem' }}>
-              <InputLabel shrink={true}>Password</InputLabel>
-              <Input
-                type="password"
-                name="password"
-                value={props.values.password}
-                onChange={props.handleChange}
-              />
-            </FormControl>
-            <Button type="submit">Create</Button>
+          <form onSubmit={props.handleSubmit}>
+            <div style={styles.formContainer}>
+              <FormControl style={styles.formControl}>
+                <InputLabel shrink={true}>Your name</InputLabel>
+                <Input
+                  type="text"
+                  name="fullName"
+                  value={props.values.fullName}
+                  onChange={props.handleChange}
+                />
+              </FormControl>
+              <FormControl style={styles.formControl}>
+                <InputLabel shrink={true}>Email Address</InputLabel>
+                <Input
+                  type="email"
+                  name="email"
+                  value={props.values.email}
+                  onChange={props.handleChange}
+                />
+              </FormControl>
+              <FormControl style={styles.formControl}>
+                <InputLabel shrink={true}>Password</InputLabel>
+                <Input
+                  type="password"
+                  name="password"
+                  value={props.values.password}
+                  onChange={props.handleChange}
+                />
+              </FormControl>
+            </div>
+            <Button style={styles.button} type="submit">
+              Create
+            </Button>
           </form>
         )
       }}
