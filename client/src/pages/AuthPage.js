9f9cde6d0c1537b5f8dc4716aa270b6c7569a63d
Leon <leon.karho.chung@gmail.com>
2019-11-06 11:26:07 -0500

    auth wrapper component: to avoid background image rerendering and to allow dynamic Form components mounting depending on url path

---
+++ client/src/pages/AuthPage.js
@@ -0,0 +1,51 @@
+import React from "react";
+import { Link } from "react-router-dom";
+import SignIn from "./SignIn";
+import SignUp from "./SignUp";
+
+const styles = {
+  link: {
+    borderRadius: "9999px",
+    display: "inline-block",
+    textDecoration: "none",
+    backgroundColor: "white",
+    color: "black",
+    paddingTop: "0.5rem",
+    paddingBottom: "0.5rem",
+    paddingLeft: "1rem",
+    paddingRight: "1rem",
+    marginBottom: ".5rem",
+    fontSize: "1rem",
+    position: "absolute",
+    right: "3rem",
+    top: "3rem"
+  }
+};
+
+const AuthPage = ({ location }) => {
+  const { pathname } = location;
+  return (
+    <div style={{ display: "flex", height: "100vh" }}>
+      {pathname === "/signup" ? (
+        <SignIn />
+      ) : pathname === "/signin" ? (
+        <SignUp />
+      ) : null}
+      <section
+        style={{ backgroundColor: "black", height: "100%", width: "50%" }}
+      >
+        {pathname === "/signup" ? (
+          <Link style={styles.link} to="/signin">
+            Sign up
+          </Link>
+        ) : pathname === "/signin" ? (
+          <Link style={styles.link} to="/signup">
+            Sign up
+          </Link>
+        ) : null}
+      </section>
+    </div>
+  );
+};
+
+export default AuthPage;
