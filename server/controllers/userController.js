355e8275709b98a4fb99786aeb712c07cffeef93
kay <kaynguyen.dev@gmail.com>
2019-11-05 19:24:01 -0500

    config multer; created update user middleware functions

--- server/controllers/userController.js
+++ server/controllers/userController.js
@@ -1,4 +1,15 @@
 import User from "../models/userModel";
+import multerUpload from "../utils/multerUpload";
+
+export const uploadUserAvatar = multerUpload.single("avatar");
+
+const filterObj = (obj, ...allowedFields) => {
+  let newObj = {};
+  Object.keys(obj).forEach(key => {
+    if (allowedFields.includes(key)) newObj[key] = obj[key];
+  });
+  return newObj;
+};
 
 export const getUser = async (req, res, next) => {
   const user = await User.findById(req.params.id);
@@ -17,4 +28,25 @@ export const getUser = async (req, res, next) => {
 export const getMe = (req, res, next) => {
   req.params.id = req.user.id;
   next();
+};
+
+export const updateMe = async (req, res, next) => {
+  if (req.body.password)
+    res.status(400).json({
+      status: "failure",
+      message: "This route is not for password updates"
+    });
+
+  const filteredBody = filterObj(req.body, "name");
+  if (req.file) filteredBody.avatar = req.file.filename;
+
+  const updatedUser = await User.findByIdAndUpdate(req.user.id, filteredBody, {
+    new: true,
+    runValidators: true
+  });
+
+  res.status(200).json({
+    status: "success",
+    data: { user: updatedUser }
+  });
 };
--- server/routes/userRouter.js
+++ server/routes/userRouter.js
@@ -1,7 +1,5 @@
 import express from "express";
-import multer from "multer";
 
-const upload = multer({ dest: "public/img/users" });
 const router = express.Router();
 
 import * as authController from "../controllers/authController";
@@ -11,5 +9,10 @@ router.post("/signup", authController.signup);
 router.post("/login", authController.login);
 router.use(authController.checkAuth);
 router.get("/profile", userController.getMe, userController.getUser);
+router.patch(
+  "/profile/update",
+  userController.uploadUserAvatar,
+  userController.updateMe
+);
 
 export default router;
