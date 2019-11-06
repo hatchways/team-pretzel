5a02dd5494dbf3f20e64b22d4cf203595d89a812
Leon <leon.karho.chung@gmail.com>
2019-11-06 12:28:04 -0500

    add redirect when app loads on /

--- client/src/App.js
+++ client/src/App.js
@@ -1,6 +1,6 @@
 import React from "react";
 import { MuiThemeProvider, CssBaseline } from "@material-ui/core";
-import { BrowserRouter, Route } from "react-router-dom";
+import { BrowserRouter, Route, Redirect } from "react-router-dom";
 import "./App.css";
 import { theme } from "./themes/theme";
 import AuthPage from "./pages/AuthPage";
@@ -10,6 +10,9 @@ function App() {
     <MuiThemeProvider theme={theme}>
       <BrowserRouter>
         <CssBaseline />
+        <Route exact path="/">
+          <Redirect to="/signin" />
+        </Route>
         <Route exact path={["/signin", "/signup"]} component={AuthPage} />
       </BrowserRouter>
     </MuiThemeProvider>
