9f07b32de0093ea3e6878533df0d0141336785eb
kay <kaynguyen.dev@gmail.com>
2019-11-07 15:35:45 -0500

    Merge branch 'feature/enhance-multer-uploads' into feature/create-polls

--- server/models/User.js
+++ server/models/User.js
@@ -4,6 +4,10 @@ import bcrypt from "bcryptjs";
 
 const userSchema = new mongoose.Schema(
   {
+    __v: {
+      type: Number,
+      select: false
+    },
     name: {
       type: String,
       required: [true, "Please tell us your name"]
--- server/routes/userRouter.js
+++ server/routes/userRouter.js
@@ -6,20 +6,20 @@ import * as authController from "../controllers/authController";
 import * as userController from "../controllers/userController";
 import * as pollController from "../controllers/pollController";
 
+router.get("/", userController.getAllUsers);
+// router.get("/:id", userController.getUser);
+
 router.post("/signup", authController.signup);
 router.post("/login", authController.login);
 
-// test //
-router.get("/all-polls", pollController.getAllPolls);
-router.get("/all-users", userController.getAllUsers);
-//test //
-
+// Protected routes
 router.use(authController.checkAuth);
 router.get("/profile", userController.getMe, userController.getUser);
 router.patch(
   "/profile/update",
   userController.uploadUserAvatar,
-  userController.resizeUserAvatar,
+  userController.saveUserAvatar,
+  // userController.resizeUserAvatar,
   userController.updateMe
 );
 router.post("/new-poll", pollController.createPoll);
