e37ae7a730193b9036af6fc6d01ad8b3ed1908a0
Leon <leon.karho.chung@gmail.com>
2019-11-07 12:37:25 -0500

    export inline style obj

---
+++ client/src/pages/presentational/inlineStyles.js
@@ -0,0 +1,16 @@
+export const styles = {
+  formContainer: { display: "flex", flexDirection: "column" },
+  button: {
+    borderRadius: "9999px",
+    display: "inline-block",
+    textDecoration: "none",
+    backgroundColor: "#111",
+    color: "white",
+    paddingTop: "0.5rem",
+    paddingBottom: "0.5rem",
+    paddingLeft: "1rem",
+    paddingRight: "1rem",
+    marginBottom: ".5rem",
+    fontSize: "1rem"
+  }
+};
