71dfa8f5d97e7377777aa6a91bcc6b03b1cacf93
Leon <leon.karho.chung@gmail.com>
2019-11-10 14:25:00 -0500

    remove friendlist test route

--- server/routes/friendListRouter.js
+++ server/routes/friendListRouter.js
@@ -3,11 +3,6 @@ const router = express.Router();
 import * as authController from "../controllers/authController";
 import { createFriendList } from "../controllers/friendlistController";
 
-// test friendlist route
-router.get("/", (req, res) => {
-  return res.json({ msg: "this is the friendlist route" });
-});
-
 // create a friendlist
 router.use(authController.checkAuth);
 router.post("/create", createFriendList);
