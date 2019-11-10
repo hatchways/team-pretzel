dc195b53c715c539ecc9140b107e075ae48958d9
Leon <leon.karho.chung@gmail.com>
2019-11-10 14:23:36 -0500

    export into controller

--- server/routes/friendListRouter.js
+++ server/routes/friendListRouter.js
@@ -1,7 +1,7 @@
 import express from "express";
 const router = express.Router();
-import FriendList from "../models/FriendList";
 import * as authController from "../controllers/authController";
+import { createFriendList } from "../controllers/friendlistController";
 
 // test friendlist route
 router.get("/", (req, res) => {
@@ -10,24 +10,6 @@ router.get("/", (req, res) => {
 
 // create a friendlist
 router.use(authController.checkAuth);
-router.post("/create", async (req, res) => {
-  // accept title + friends = array of ids + user (owner) id from jwt token
-  const { title, friendIds } = req.body;
-  const { id } = req.user;
-  console.log(id);
-  try {
-    const newFriendList = await FriendList.create({
-      title,
-      friends: [...friendIds],
-      user: id
-    });
-
-    newFriendList.save();
-
-    return res.json(newFriendList);
-  } catch (error) {
-    console.log(error);
-  }
-});
+router.post("/create", createFriendList);
 
 export default router;
