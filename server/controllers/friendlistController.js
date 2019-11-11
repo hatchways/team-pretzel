835b48bd81252b953e6eb25b09410c8aae9b02e9
Leon <leon.karho.chung@gmail.com>
2019-11-10 20:55:59 -0500

    createFriendList route

--- server/controllers/friendlistController.js
+++ server/controllers/friendlistController.js
@@ -1,14 +1,18 @@
 import FriendList from "../models/FriendList";
 
 export const createFriendList = async (req, res) => {
-  const { friendIds } = req.body;
+  const { friendIds, title, user } = req.body;
   if (friendIds.length === 0) {
     res
       .status(400)
       .json({ status: "failure", message: "No friends were selected" });
   }
 
-  const newFriendList = await FriendList.create(req.body);
+  const newFriendList = await FriendList.create({
+    title,
+    friends: [...friendIds],
+    user
+  });
   res
     .status(201)
     .json({ status: "success", data: { friendList: newFriendList } });
