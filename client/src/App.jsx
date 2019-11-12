5aca46f58ca1ad96eeed207138893eeb297717c1
Leon <leon.karho.chung@gmail.com>
2019-11-12 12:27:43 -0500

    add loggedIn state - if token exists on mount > redirect to dashboard

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -1,4 +1,4 @@
-import React from "react";
+import React, { useState, useEffect } from "react";
 import { MuiThemeProvider, CssBaseline } from "@material-ui/core";
 import { BrowserRouter, Route, Redirect } from "react-router-dom";
 import { theme } from "./themes/theme";
@@ -6,13 +6,22 @@ import AuthPage from "./pages/AuthPage";
 import Dashboard from "./pages/Dashboard";
 
 const App = () => {
+  const [loggedIn, setLoggedIn] = useState(false);
+
+  useEffect(() => {
+    const token = localStorage.getItem("jwtToken");
+    if (token) {
+      setLoggedIn(true);
+    }
+  }, []);
+
   return (
     <MuiThemeProvider theme={theme}>
       <BrowserRouter>
         <CssBaseline />
-        {/*    <Route exact path="/">
-          <Redirect to="/signin" />
-        </Route> */}
+        <Route exact path="/">
+          {loggedIn ? <Redirect to="/dashboard" /> : <Redirect to="/signin" />}
+        </Route>
         <Route exact path={["/signin", "/signup"]} component={AuthPage} />
         <Route exact path="/dashboard" component={Dashboard} />
       </BrowserRouter>
