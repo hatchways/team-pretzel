3dae47583d7f2f157996fca48d6f6a644fc70c5d
Leon <leon.karho.chung@gmail.com>
2019-11-15 14:56:27 -0500

    if token = true -> set token to auth headers

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -1,29 +1,19 @@
 import React, { useState, useEffect } from "react";
-import axios from "axios";
-
 import { MuiThemeProvider, CssBaseline } from "@material-ui/core";
 import { BrowserRouter, Route, Redirect } from "react-router-dom";
 import { theme } from "./themes/theme";
 import AuthPage from "./pages/AuthPage";
 import Dashboard from "./pages/Dashboard";
-
-const setAuthToken = token => {
-  if (token) {
-    // apply to every req
-    axios.defaults.headers.common["Authorization"] = `Bearer ${token}`;
-  }
-};
+import { setAuthToken } from "./utils/helpers";
 
 const App = () => {
   const [loggedIn, setLoggedIn] = useState(false);
 
   useEffect(() => {
-    if (localStorage) {
-      const token = localStorage.getItem("jwtToken");
-      if (token) {
-        setAuthToken(token);
-        setLoggedIn(true);
-      }
+    // check if jwt in localstorage
+    if (localStorage.jwtToken) {
+      setAuthToken(localStorage.jwtToken);
+      setLoggedIn(true);
     }
   }, []);
 
--- client/src/pages/Dashboard.jsx
+++ client/src/pages/Dashboard.jsx
@@ -1,11 +1,18 @@
 import React from "react";
 import AppBarDrawer from "./container/AppBarDrawer";
+import { setAuthToken } from "../utils/helpers";
 
 class Dashboard extends React.Component {
   handleLogOut = () => {
     localStorage.removeItem("jwtToken");
     this.props.history.push("/signin");
   };
+
+  componentDidMount() {
+    if (localStorage.jwtToken) {
+      setAuthToken(localStorage.jwtToken);
+    }
+  }
 
   render() {
     return (
