f624a7106ef1d23e10cd29bd59d42638087f946e
m3tron <sunny.heyar@gmail.com>
2019-11-15 13:00:24 -0500

    Applied requested changes
    - Changed file type to .jsx
    - Removed Navbar and Dashboard because changes were already merged into AppBarDrawer and Dashboard.js

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -5,7 +5,7 @@ import { MuiThemeProvider, CssBaseline } from "@material-ui/core";
 import { BrowserRouter, Route, Redirect } from "react-router-dom";
 import { theme } from "./themes/theme";
 import AuthPage from "./pages/AuthPage";
-import Dashboard from "./pages/Dashboard.js";
+import Dashboard from "./pages/Dashboard";
 
 const setAuthToken = token => {
   if (token) {
