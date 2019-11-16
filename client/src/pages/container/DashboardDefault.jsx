195c1d247101504f86c84806a60ace8ee8a0fce5
Leon <leon.karho.chung@gmail.com>
2019-11-16 17:40:07 -0500

    styling and display create friend list button & friend list cards

--- client/src/pages/container/DashboardDefault.jsx
+++ client/src/pages/container/DashboardDefault.jsx
@@ -1,7 +1,27 @@
 import React from "react";
+import { Container, Typography } from "@material-ui/core";
+import FriendListCard from "../presentational/FriendListCard";
+import FriendlistDialog from "../presentational/FriendlistDialog";
 
 const DashboardDefault = props => {
-  return <div>this is the main content with polls and friendlist</div>;
+  return (
+    <>
+      <Container style={{ marginTop: "1rem" }}>
+        <div
+          style={{
+            display: "flex",
+            alignItems: "center",
+            justifyContent: "space-between"
+          }}
+        >
+          <Typography>Friend Lists</Typography>
+          <FriendlistDialog />
+        </div>
+        <FriendListCard />
+      </Container>
+      <div>Polls</div>
+    </>
+  );
 };
 
 export default DashboardDefault;
