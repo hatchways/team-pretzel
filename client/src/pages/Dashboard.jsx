b5179b877514aa3ac394032f883ac29c475f8f91
Leon <leon.karho.chung@gmail.com>
2019-11-16 15:27:09 -0500

    implement content container comp

--- client/src/pages/Dashboard.jsx
+++ client/src/pages/Dashboard.jsx
@@ -1,6 +1,7 @@
 import React, { useEffect } from "react";
 import AppBarDrawer from "./container/AppBarDrawer";
 import { setAuthToken } from "../utils/helpers";
+import ContentContainer from "./container/ContentContainer";
 
 const Dashboard = ({ history }) => {
   const handleLogOut = () => {
@@ -17,6 +18,9 @@ const Dashboard = ({ history }) => {
   return (
     <>
       <AppBarDrawer handleLogOut={handleLogOut} />
+      <ContentContainer>
+        <div>this is another component</div>
+      </ContentContainer>
     </>
   );
 };
