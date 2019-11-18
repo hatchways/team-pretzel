76bf4bda4ac291c4c22f4580f30793ba4ab01237
Leon <leon.karho.chung@gmail.com>
2019-11-18 13:31:12 -0500

    ignore

--- client/src/pages/presentational/PollDialog.jsx
+++ client/src/pages/presentational/PollDialog.jsx
@@ -119,10 +119,7 @@ const PollDialog = () => {
                     multiple
                     type="file"
                   />
-                  {values.images[0] ? (
-                    <img id="output1" alt="upload #1 " />
-                  ) : null}
-
+                  <img id="output1" alt="upload #1 " />
                   <input
                     onChange={event => {
                       setFieldValue("images", [
@@ -138,9 +135,7 @@ const PollDialog = () => {
                     multiple
                     type="file"
                   />
-                  {values.images[1] ? (
-                    <img id="output2" alt="upload #2 " />
-                  ) : null}
+                  <img id="output2" alt="upload #2 " />
                   <label htmlFor="upload-button-1">
                     <Button component="span">Upload image 1</Button>
                   </label>
