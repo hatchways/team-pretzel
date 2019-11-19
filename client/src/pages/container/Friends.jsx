2729c611d69f4a9c1111bac7952777d302ff50ca
Leon <leon.karho.chung@gmail.com>
2019-11-19 15:46:40 -0500

    friends page - display tabs

--- client/src/pages/container/Friends.jsx
+++ client/src/pages/container/Friends.jsx
@@ -1,7 +1,41 @@
-import React from "react";
+import React, { useState } from "react";
+import { makeStyles, Paper, Tabs, Tab, Typography } from "@material-ui/core";
+import FriendsTabPanel from "../presentational/FriendsTabPanel";
+
+const useStyles = makeStyles({
+  root: {
+    width: "80%"
+  }
+});
 
 const Friends = params => {
-  return <div>this is the friends component</div>;
+  const classes = useStyles();
+
+  const [value, setValue] = useState(0);
+  const handleChangeTab = (event, newValue) => {
+    setValue(newValue);
+  };
+
+  return (
+    <div className={classes.root}>
+      <Typography>Friends</Typography>
+      <Paper>
+        <div>
+          <Tabs
+            centered
+            variant="fullWidth"
+            value={value}
+            onChange={handleChangeTab}
+          >
+            <Tab label="Following" />
+            <Tab label="Suggested" />
+          </Tabs>
+        </div>
+        <FriendsTabPanel value={value} index={0} />
+        <FriendsTabPanel value={value} index={1} />
+      </Paper>
+    </div>
+  );
 };
 
 export default Friends;
