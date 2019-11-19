6893b57491375059ac6a2999143a91d001b5c2c3
Leon <leon.karho.chung@gmail.com>
2019-11-19 15:47:21 -0500

    tab panel for list of friends

---
+++ client/src/pages/presentational/FriendsTabPanel.jsx
@@ -0,0 +1,65 @@
+import React from "react";
+import {
+  makeStyles,
+  Avatar,
+  Button,
+  List,
+  ListItem,
+  ListItemAvatar,
+  Typography,
+  ListItemText,
+  ListItemSecondaryAction
+} from "@material-ui/core";
+
+const dummyFriends = [
+  {
+    id: 1,
+    name: "leon",
+    avatar: "#"
+  },
+  {
+    id: 2,
+    name: "key",
+    avatar: "#"
+  },
+  {
+    id: 3,
+    name: "sunny",
+    avatar: "#"
+  }
+];
+
+const useStyles = makeStyles({
+  root: {
+    padding: "0"
+  },
+  listItem: {
+    borderBottom: "0.5px solid lightgrey",
+    padding: "1rem 2rem"
+  }
+});
+
+const FriendsTabPanel = ({ value, index }) => {
+  const classes = useStyles();
+  return (
+    <List className={classes.root} hidden={value !== index}>
+      {dummyFriends.map(friend => {
+        return (
+          <ListItem className={classes.listItem} key={friend.id}>
+            <ListItemAvatar>
+              <Avatar alt={friend.name} src={friend.avatar} />
+            </ListItemAvatar>
+            <ListItemText>
+              <Typography>{friend.name}</Typography>
+            </ListItemText>
+            <ListItemSecondaryAction>
+              <Button>Follow</Button>
+            </ListItemSecondaryAction>
+          </ListItem>
+        );
+      })}
+    </List>
+  );
+};
+
+export default FriendsTabPanel;
