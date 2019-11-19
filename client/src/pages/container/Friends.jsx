87abcb04deac14109117b9f9f45ebdf6206918d1
Leon <leon.karho.chung@gmail.com>
2019-11-19 17:51:31 -0500

    fetch friend and potentials - pass data into respective panels - styling

--- client/src/pages/container/Friends.jsx
+++ client/src/pages/container/Friends.jsx
@@ -1,15 +1,33 @@
 import React, { useState } from "react";
 import { makeStyles, Paper, Tabs, Tab, Typography } from "@material-ui/core";
 import FriendsTabPanel from "../presentational/FriendsTabPanel";
+import SuggestedTabPanel from "../presentational/SuggestedTabPanel";
+import useGet from "../../utils/hooks/useGet";
 
 const useStyles = makeStyles({
   root: {
-    width: "80%"
+    height: "100%",
+    minWidth: "80%",
+    maxWidth: "80%",
+    textAlign: "center",
+    display: "flex",
+    flexDirection: "column",
+    justifyContent: "center",
+    margin: "auto"
   }
 });
 
-const Friends = params => {
+const Friends = ({ location }) => {
+  const { user } = location.state;
   const classes = useStyles();
+
+  // get friends
+  const friends = useGet(`/api/v1/friends/${user.id}`, "friends");
+  // get suggested friends
+  const potentialFriends = useGet(
+    `/api/v1/friends/suggested-friends/${user.id}`,
+    "potentialFriends"
+  );
 
   const [value, setValue] = useState(0);
   const handleChangeTab = (event, newValue) => {
@@ -31,8 +49,8 @@ const Friends = params => {
             <Tab label="Suggested" />
           </Tabs>
         </div>
-        <FriendsTabPanel value={value} index={0} />
-        <FriendsTabPanel value={value} index={1} />
+        <FriendsTabPanel data={friends} value={value} index={0} />
+        <SuggestedTabPanel data={potentialFriends} value={value} index={1} />
       </Paper>
     </div>
   );
