83e7c7155d90e830116072648e3fe44827021375
Leon <leon.karho.chung@gmail.com>
2019-11-19 17:52:56 -0500

    potential friends tab panel

---
+++ client/src/pages/presentational/SuggestedTabPanel.jsx
@@ -0,0 +1,51 @@
+import React from "react";
+import {
+  makeStyles,
+  Avatar,
+  Button,
+  List,
+  ListItem,
+  ListItemAvatar,
+  Typography,
+  ListItemText,
+  ListItemSecondaryAction
+} from "@material-ui/core";
+
+const useStyles = makeStyles({
+  root: {
+    padding: "0"
+  },
+  listItem: {
+    borderBottom: "0.5px solid lightgrey",
+    padding: "1rem 2rem"
+  }
+});
+
+const SuggestedTabPanel = ({ value, index, data }) => {
+  const classes = useStyles();
+  return (
+    <List className={classes.root} hidden={value !== index}>
+      {data === null ? (
+        <div>...loading...</div>
+      ) : (
+        data.map(potential => {
+          return (
+            <ListItem className={classes.listItem} key={potential.id}>
+              <ListItemAvatar>
+                <Avatar alt={potential.name} src={potential.avatar} />
+              </ListItemAvatar>
+              <ListItemText>
+                <Typography>{potential.name}</Typography>
+              </ListItemText>
+              <ListItemSecondaryAction>
+                <Button>Follow</Button>
+              </ListItemSecondaryAction>
+            </ListItem>
+          );
+        })
+      )}
+    </List>
+  );
+};
+
+export default SuggestedTabPanel;
