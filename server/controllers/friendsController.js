eb6864dc186417ad1524c68cf0841729daf59386
Leon <leon.karho.chung@gmail.com>
2019-11-29 02:26:33 -0500

    populate friends field and return array

--- server/controllers/friendsController.js
+++ server/controllers/friendsController.js
@@ -17,10 +17,12 @@ export const getAllFriends = catchAsync(async (req, res, next) => {
 // Add or remove a friend
 export const updateFriends = catchAsync(async (req, res, next) => {
   const self = await User.findById(req.user.id);
-  let friends = await Friends.findOne({ user: req.user.id });
+  let friends = await Friends.findOne({ user: req.user.id }).populate(
+    "friends"
+  );
 
   if (!friends) {
-    friends = await Friends.create({ user: req.user.id });
+    friends = await Friends.create({ user: req.user.id }).populate("friends");
   }
 
   friends.befriend(req.params.userId);
@@ -28,7 +30,7 @@ export const updateFriends = catchAsync(async (req, res, next) => {
 
   res.status(201).json({
     status: "success",
-    friends
+    friends: friends.friends
   });
 });
 
