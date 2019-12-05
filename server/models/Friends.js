68371f2f356d5493ffc962a9fe051c9090c4a1ab
kay <kaynguyen.dev@gmail.com>
2019-12-05 16:30:41 -0500

    remove version querying on Friends schema

--- server/models/Friends.js
+++ server/models/Friends.js
@@ -10,6 +10,10 @@ const friendsSchema = new mongoose.Schema({
   user: {
     type: mongoose.Schema.Types.ObjectId,
     ref: "User"
+  },
+  __v: {
+    type: Number,
+    select: false
   }
 });
 
